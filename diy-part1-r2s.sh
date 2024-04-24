a7323b0c9f42167a75170f7f3a50ec9bb1328a80
x2358473 <649056978@qq.com>
2024-04-24 08:50:20 +0800

    Update diy-part1-r2s.sh

--- diy-part1-r2s.sh
+++ diy-part1-r2s.sh
@@ -13,8 +13,15 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # r2s 回退版本，暂时处理办法
+git config --global user.name "luobo"
+git config --global user.email "649056978@qq.com"
+# 不提交，只储藏
+git stash
+# 回退版本，否则无法启动
 git revert f788f35 --no-edit
 git revert 3b68197 --no-edit
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# 解封hellword
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
